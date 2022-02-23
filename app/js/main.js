e60ff8ffa4aca7d80b0370b6f82c6d2177c7c9cf
vsulimovvv <super.sulimov2014@yandex.ru>
2022-02-24 01:33:19 +0300

    поправил пару страниц

--- app/js/main.js
+++ app/js/main.js
@@ -65,10 +65,6 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     let mySwiper = new Swiper('#slider-main', {
-      // navigation: {
-      //   nextEl: arrowNext,
-      //   prevEl: arrowPrev,
-      // },
       spaceBetween: 10,
       loopedSlides: 4,
       thumbs: {
@@ -187,51 +183,51 @@ window.addEventListener('DOMContentLoaded', () => {
   // })();
 
   // * ===== Toggle Tabs
-  // function someTabs(headerSelector, tabSelector, contentSelector, activeClass) {
-  //   const header = document.querySelectorAll(headerSelector);
-  //   const tab = document.querySelectorAll(tabSelector);
-  //   const content = document.querySelectorAll(contentSelector);
+  function someTabs(headerSelector, tabSelector, contentSelector, activeClass) {
+    const header = document.querySelectorAll(headerSelector);
+    const tab = document.querySelectorAll(tabSelector);
+    const content = document.querySelectorAll(contentSelector);
 
-  //   if (header) {
-  //     hideTabContent();
-  //     showTabContent();
+    if (header) {
+      hideTabContent();
+      showTabContent();
 
-  //     function hideTabContent() {
-  //       content.forEach((item) => {
-  //         item.classList.remove('active');
-  //       });
-  //       tab.forEach((item) => {
-  //         item.classList.remove(activeClass);
-  //       });
-  //     }
+      function hideTabContent() {
+        content.forEach((item) => {
+          item.classList.remove('active');
+        });
+        tab.forEach((item) => {
+          item.classList.remove(activeClass);
+        });
+      }
 
-  //     function showTabContent(i = 0) {
-  //       content[i].classList.add('active');
-  //       tab[i].classList.add(activeClass);
-  //     }
+      function showTabContent(i = 0) {
+        content[i].classList.add('active');
+        tab[i].classList.add(activeClass);
+      }
 
-  //     header.forEach((item) => {
-  //       if (item) {
-  //         item.addEventListener('click', (e) => {
-  //           const target = e.target;
+      header.forEach((item) => {
+        if (item) {
+          item.addEventListener('click', (e) => {
+            const target = e.target;
 
-  //           if (target.classList.contains(tabSelector.replace(/\./, ''))) {
-  //             tab.forEach((item, i) => {
-  //               if (target == item || target.parentNode == item) {
-  //                 hideTabContent();
-  //                 showTabContent(i);
-  //               }
-  //             });
-  //           }
-  //         });
-  //       }
-  //     });
-  //   }
-  // }
-  // someTabs(
-  //   '.tabs-spec',
-  //   '.tabs-spec__btn',
-  //   '.tabs-spec__content',
-  //   'tabs-spec__btn--active'
-  // );
+            if (target.classList.contains(tabSelector.replace(/\./, ''))) {
+              tab.forEach((item, i) => {
+                if (target == item || target.parentNode == item) {
+                  hideTabContent();
+                  showTabContent(i);
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  }
+  someTabs(
+    '.tabs-spec',
+    '.tabs-spec__btn',
+    '.tabs-spec__content',
+    'tabs-spec__btn--active'
+  );
 });
